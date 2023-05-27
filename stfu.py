b1cbf69ad50b107a214f299ca49dee2af96aa41f
Rick <81345344+rivkxx@users.noreply.github.com>
2023-05-27 17:50:06 +0530

    Create stfu.py

---
+++ stfu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == "quit":
+            break
+        else:
+            print("shut up")
+
+if __name__ == "__main__":
+    main()
